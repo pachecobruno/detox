ee004354f10babad93c1117fa483a6aac322eb10
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-05-08 19:39:03 +0200

    apply prettier on generation

--- generation/helpers.js
+++ generation/helpers.js
@@ -1,23 +1,21 @@
-const t = require("babel-types");
+const t = require('babel-types');
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function methodNameToSnakeCase(name) {
-  return name
-          .split(':')
-          .map((item, index) => 
-            index === 0 ? item : capitalizeFirstLetter(item)
-          ).join('');
+	return name
+		.split(':')
+		.map((item, index) => (index === 0 ? item : capitalizeFirstLetter(item)))
+		.join('');
 }
 
 function callGlobal(sanitizerName) {
-  return argIdentifier =>
-    t.callExpression(t.identifier(sanitizerName), [t.identifier(argIdentifier)]);
+	return (argIdentifier) => t.callExpression(t.identifier(sanitizerName), [t.identifier(argIdentifier)]);
 }
 
 module.exports = {
-  methodNameToSnakeCase,
-  callGlobal
+	methodNameToSnakeCase,
+	callGlobal
 };
--- generation/index.js
+++ generation/index.js
@@ -1,38 +1,27 @@
 #!/usr/bin/env node
-const prettier = require("prettier");
-const fs = require("fs");
+const prettier = require('prettier');
+const fs = require('fs');
 
-const generateIOSAdapters = require("./adapters/ios");
+const generateIOSAdapters = require('./adapters/ios');
 const iosFiles = {
-	"../detox/ios/EarlGrey/EarlGrey/Action/GREYActions.h":
-		"../detox/src/ios/earlgreyapi/GREYActions.js",
-	"../detox/ios/Detox/GREYMatchers+Detox.h":
-		"../detox/src/ios/earlgreyapi/GREYMatchers+Detox.js",
-	"../detox/ios/EarlGrey/EarlGrey/Matcher/GREYMatchers.h":
-		"../detox/src/ios/earlgreyapi/GREYMatchers.js",
-	"../detox/ios/EarlGrey/EarlGrey/Core/GREYInteraction.h":
-		"../detox/src/ios/earlgreyapi/GREYInteraction.js",
-	"../detox/ios/Detox/GREYCondition+Detox.h":
-		"../detox/src/ios/earlgreyapi/GREYConditionDetox.js",
-	"../detox/ios/EarlGrey/EarlGrey/Synchronization/GREYCondition.h":
-		"../detox/src/ios/earlgreyapi/GREYCondition.js",
-	"../detox/ios/Detox/GREYConfiguration+Detox.h":
-		"../detox/src/ios/earlgreyapi/GREYConfigurationDetox.js",
-	"../detox/ios/EarlGrey/EarlGrey/Common/GREYConfiguration.h":
-		"../detox/src/ios/earlgreyapi/GREYConfiguration.js"
+	'../detox/ios/EarlGrey/EarlGrey/Action/GREYActions.h': '../detox/src/ios/earlgreyapi/GREYActions.js',
+	'../detox/ios/Detox/GREYMatchers+Detox.h': '../detox/src/ios/earlgreyapi/GREYMatchers+Detox.js',
+	'../detox/ios/EarlGrey/EarlGrey/Matcher/GREYMatchers.h': '../detox/src/ios/earlgreyapi/GREYMatchers.js',
+	'../detox/ios/EarlGrey/EarlGrey/Core/GREYInteraction.h': '../detox/src/ios/earlgreyapi/GREYInteraction.js',
+	'../detox/ios/Detox/GREYCondition+Detox.h': '../detox/src/ios/earlgreyapi/GREYConditionDetox.js',
+	'../detox/ios/EarlGrey/EarlGrey/Synchronization/GREYCondition.h': '../detox/src/ios/earlgreyapi/GREYCondition.js',
+	'../detox/ios/Detox/GREYConfiguration+Detox.h': '../detox/src/ios/earlgreyapi/GREYConfigurationDetox.js',
+	'../detox/ios/EarlGrey/EarlGrey/Common/GREYConfiguration.h': '../detox/src/ios/earlgreyapi/GREYConfiguration.js'
 };
 
 generateIOSAdapters(iosFiles);
 const externalFilesToDownload = {
-	"android.support.test.espresso.action.ViewActions":
-		"../detox/src/android/espressoapi/ViewActions.js"
+	'android.support.test.espresso.action.ViewActions': '../detox/src/android/espressoapi/ViewActions.js'
 };
 
-const generateAndroidAdapters = require("./adapters/android");
-const downloadEspressoFileByClass = require("./utils/downloadEspresso");
-const downloadedAndroidFilesMap = Object.entries(
-	externalFilesToDownload
-).reduce(
+const generateAndroidAdapters = require('./adapters/android');
+const downloadEspressoFileByClass = require('./utils/downloadEspresso');
+const downloadedAndroidFilesMap = Object.entries(externalFilesToDownload).reduce(
 	(obj, [fullyQualifiedClass, dest]) => ({
 		...obj,
 		[downloadEspressoFileByClass(fullyQualifiedClass)]: dest
@@ -41,20 +30,16 @@ const downloadedAndroidFilesMap = Object.entries(
 );
 const androidFiles = {
 	...downloadedAndroidFilesMap,
-	"../detox/android/detox/src/main/java/com/wix/detox/espresso/DetoxAction.java":
-		"../detox/src/android/espressoapi/DetoxAction.js",
-	"../detox/android/detox/src/main/java/com/wix/detox/espresso/DetoxMatcher.java":
-		"../detox/src/android/espressoapi/DetoxMatcher.js"
+	'../detox/android/detox/src/main/java/com/wix/detox/espresso/DetoxAction.java': '../detox/src/android/espressoapi/DetoxAction.js',
+	'../detox/android/detox/src/main/java/com/wix/detox/espresso/DetoxMatcher.java': '../detox/src/android/espressoapi/DetoxMatcher.js'
 };
 generateAndroidAdapters(androidFiles);
-
 
 // Run prettier over the generated files
 const allFiles = [...Object.values(iosFiles), ...Object.values(androidFiles)];
 
-allFiles.forEach(file => {
-    const text = fs.readFileSync(file, "utf8");
-    const formatted = prettier.format(text);
-    fs.writeFileSync(file, formatted);
+allFiles.forEach((file) => {
+	const text = fs.readFileSync(file, 'utf8');
+	const formatted = prettier.format(text);
+	fs.writeFileSync(file, formatted);
 });
-
